df837c2781d2c698cef170ec29c2e832ef2d24ed
abhishek <abhishekkulkarni706@gmail.com>
2021-01-06 16:32:11 +0530

    webpack changes

--- django_react_chat/main/settings.py
+++ django_react_chat/main/settings.py
@@ -23,9 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e-s%_4r)p^f^^wx!8^#=z0zewg67cv!ve2v712pdmh5f=@jya7'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
