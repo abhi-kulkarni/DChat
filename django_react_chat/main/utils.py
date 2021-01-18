908692e976edb1e527afb4d02deeb3c984b5cd1a
abhishek <abhishekkulkarni706@gmail.com>
2021-01-18 14:48:54 +0530

    chat_fixes

--- django_react_chat/main/utils.py
+++ django_react_chat/main/utils.py
@@ -1,13 +1,13 @@
 from backend.serializers import MyTokenObtainPairSerializer, UserSerializer
-from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 from django.contrib.staticfiles.storage import ManifestFilesMixin
 
 
-class CustomS3Storage(ManifestFilesMixin, S3BotoStorage):
+class CustomS3Storage(ManifestFilesMixin, S3Boto3Storage):
     pass
 
 StaticRootS3BotoStorage = lambda: CustomS3Storage(location='static')
-MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='uploads')
+MediaRootS3BotoStorage  = lambda: S3Boto3Storage(location='uploads')
 
 def custom_jwt_response_handler(token, user=None, request=None):
 
